95a4197ab680423174b535d2ebb28f208e33bf53
Bishnusilwal1 <bishnusilwal999@gmail.com>
2022-05-09 15:47:10 +0545

    Update ProductModel.js
    Product Models

--- models/ProductModel.js
+++ models/ProductModel.js
@@ -1,9 +1,45 @@
 const mongoose = require("mongoose");
 
 const productSchema = mongoose.Schema({
+    name:{
+        type : String,
+        required: true
+    },
 
+    images:[{
+        type : String,
+    }],
+    description:{
+        type : String,
+        required: true
+    },
+    category:[{
+        type : String,
+        required: true
+    }],
+    sizes:[{
+        type : String,
+        required: true
+    }],
+
+    rating:{
+        type : Number,
+        required: true,
+        default: 0
+    },
+    price:{
+        type : Number,
+        required: true,
+        default: 0
+    },
+    countInStock:{
+        type : Number,
+        required: true,
+        default: 0
+    },
 }, {
     timestamps: true,
-})
+},)
 
-module.exports = mongoose.model("product", productSchema); 
+module.exports = mongoose.model("product", productSchema);
+export default product 
